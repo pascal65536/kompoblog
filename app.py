587f5041e4c0810c18bfe5e8ac60a4c04b524b6b
pascal65536 <pascal65536@gmail.com>
2022-04-23 12:59:15 +0700

    fix1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 from werkzeug.exceptions import abort
+import os
+import time
+import json
 import settings
 import requests
 
@@ -13,17 +16,14 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-
 @application.route("/", methods=("GET", "POST"))
 def index():   
-    # import ipdb; ipdb.sset_trace()
-    # searchword = request.args.get('key', '')    
     try:
         text = request.json()
     except Exception as e:
-        print(e)
+        print(f'{e=}')
     finally:
-        text = f'{request.method}\n{request.headers}'
+        text = f'{request.method}\n{request.headers}\n{dir(request)}'
     url = f'https://api.telegram.org/bot{settings.token}/sendMessage?parse_mode=html&text={text}&chat_id={settings.chat_id}'
     res = requests.post(url)
     return {'statusCode': 200}
