0a38a65b5c74040a9a490cc53a715632143bfdc0
pascal65536 <pascal65536@gmail.com>
2022-04-23 21:08:39 +0700

    fix1

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 from werkzeug.exceptions import abort
-import os
-import time
+import bot
 import json
-import settings
-import requests
 
 
 application = Flask(__name__)
@@ -13,29 +10,20 @@ application.config.from_pyfile("app.config")
 
 @application.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
 
 @application.route("/", methods=("GET", "POST"))
-def index():  
-    if request.method == 'POST':
-        try:
-            text = request.get_data().decode('utf-8')
-            if text:
-                text = json.loads(text)
-            # text = {"update_id":123883508, "message":{"message_id":27781,"from":{"id":157917304,"is_bot":false,"first_name":"\u0421\u0435\u0440\u0433\u0435\u0439","last_name":"\u041f\u0430\u0445\u0442\u0443\u0441\u043e\u0432","username":"pascal65536","language_code":"ru"},"chat":{"id":157917304,"first_name":"\u0421\u0435\u0440\u0433\u0435\u0439","last_name":"\u041f\u0430\u0445\u0442\u0443\u0441\u043e\u0432","username":"pascal65536","type":"private"},"date":1650698478,"text":"5689"}}
-        except Exception as e:
-            print(f'index: {e=}')
-            text = f'{request.headers}\n{request.method}'
-        finally:
-            text = text or 'empty'
-        url = f'https://api.telegram.org/bot{settings.token}/sendMessage?parse_mode=html&text={text}&chat_id={settings.chat_id}'
-        res = requests.post(url)
-        return {'statusCode': 200}
-    
-    if request.method == 'GET':
-        return render_template('404.html'), 404
-        
+def index():
+    if request.method == "POST":
+        text = request.get_data().decode("utf-8")
+        if text:
+            bot.main(json.loads(text))
+        return {"statusCode": 200}
+
+    if request.method == "GET":
+        return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
-   application.run(host='0.0.0.0')
+    application.run(host="0.0.0.0")
