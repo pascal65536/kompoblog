7eb7e83be16f3f8caa7ad6d6a2b823ff975c20ec
pascal65536 <pascal65536@gmail.com>
2022-04-26 22:51:41 +0700

    secter

--- app.py
+++ app.py
@@ -18,23 +18,16 @@ def bottle():
     if request.method in "POST":
         text = request.get_data().decode("utf-8")
         if text:
-            bot.main({'update_id': '1001674663399'})
-        return {"statusCode": 200}
-
-    if request.method == "GET":
-        return render_template("404.html"), 404
-
-
-@application.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        text = request.get_data().decode("utf-8")
-        if text:
             bot.main(json.loads(text))
         return {"statusCode": 200}
 
     if request.method == "GET":
         return render_template("404.html"), 404
+
+
+@application.route("/", methods="GET")
+def index():
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
